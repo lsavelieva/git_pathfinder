f5c864c8c1d94d7f14eb92a8efe28c91ecf2530e
Liudmyla Savelieva <lsavelieva@e2r4p9.unit.ua>
2020-01-25 18:28:58 +0200

    new functions 25_01

--- src/mx_get_index.c
+++ src/mx_get_index.c
@@ -9,3 +9,10 @@ int mx_get_index(char *s, char c1, char c2) {
     index = index == -1 ? mx_strlen(s) : index;
     return index;
 }
+
+int mx_get_index_uniq(char *vertex, char **uniq) {
+    for (int i = 0; uniq[i]; i++)
+        if (mx_strcmp(uniq[i], vertex) == 0)
+            return i;
+    return -1;
+}
--- src/mx_matrix_adjacency.c
+++ src/mx_matrix_adjacency.c
@@ -1,22 +1,34 @@
 #include "../inc/pathfinder.h"
 
-int **mx_matrix_adjacency(char **mdata, int size) {
-    int **mad = (int**)malloc(size * size * sizeof(int*));
-    int i;
-    int j;
-    int k = 2;
+static void get_dist(int **mad, char **data, char **uniq, int size) {
+    int ind1 = 0;
+    int ind2 = 0;
 
-    for (i = 0; i < size; i++)
-        mad[i][i] = 0;
+    for (int i = 0; data[i] != NULL; i += 3) {
+        for (int j = 0; j < size; j++) {
+            ind1 = mx_get_index_uniq(data[i], uniq);
+            ind2 = mx_get_index_uniq(data[i + 1], uniq);
+            mad[ind1][ind2] = mad[ind2][ind1] = atoi(data[i + 2]);
+        }
+    }
+}
+
+int **mx_matrix_adjacency(char **data, char **uniq, int size) {
+    int **mad = (int**)malloc(size * sizeof(int*));
+    int i, j;
+
     for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++)
-            mad[i][j] = MX_INF;
+        mad[i] = (int*)malloc(size * sizeof(int));
     }
     for (i = 0; i < size; i++) {
         for (j = 0; j < size; j++) {
-            mad[i][j] = mad[j][i] = mx_atoi(mdata[k]);
-            k += 3;
+            if (i == j)
+                mad[i][j] = 0;
+            else
+                mad[i][j] = MX_INF;
         }
     }
+    get_dist(mad, data, uniq, size);
+    mx_print_intarr(mad, size);
     return mad;
 }
